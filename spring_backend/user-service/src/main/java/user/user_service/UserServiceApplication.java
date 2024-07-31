0491140b9737857af3329e591baf3c606593e76a
DOHA <koo9811@gmail.com>
2024-07-31 22:53:07 +0900

    daily init

--- spring_backend/user-service/src/main/java/user/user_service/UserServiceApplication.java
+++ spring_backend/user-service/src/main/java/user/user_service/UserServiceApplication.java
@@ -1,13 +1,13 @@
-package spring.com.spring_backend;
+package user.user_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBackendApplication {
+public class UserServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBackendApplication.class, args);
+		SpringApplication.run(UserServiceApplication.class, args);
 	}
 
 }
--- spring_backend/user-service/src/test/java/user/user_service/UserServiceApplicationTests.java
+++ spring_backend/user-service/src/test/java/user/user_service/UserServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package spring.com.spring_backend;
+package user.user_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringBackendApplicationTests {
+class UserServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
